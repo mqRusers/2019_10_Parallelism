e4141cd9885dea63062e14f2a31f227fdc44edd9
Richard Miller <richard.miller@mq.edu.au>
2019-10-24 10:27:26 +1100

    added parallel command line version

---
+++ parallel-command-line.R
@@ -0,0 +1,45 @@
+# 
+
+
+# Declare out workload function
+workload <- function (seed=0) {
+  set.seed(seed)
+  distribution <- runif(100e6)
+  average <- mean(distribution)
+  return(average)
+} 
+
+
+# make sure we have all the required packages installed
+if (!require(doParallel)) install.packages("doParallel")
+if (!require(foreach)) install.packages("foreach")
+
+# load up our libraries
+library(parallel)
+library(doParallel)
+library(foreach)
+
+# How many CPU cores do we have available on our machine
+numCores <- detectCores()
+
+# Form a cluster with all available Cores
+print(paste0("Creating Cluster (numCores=", numCores, ") ..."))
+cl <- makeCluster(numCores)
+
+# Give our fresh cluster to DoParallel
+registerDoParallel(cl)
+
+print("Starting Parallel Loop ...")
+system.time({
+  foreach (seed=1:100) %dopar% {
+    average <- workload(seed)
+    print(average)
+  } 
+})
+
+
+print("Starting Parallel Apply ...")
+system.time({
+  results = mclapply(1:10, workload, mc.cores = numCores)
+  print(results)
+})
