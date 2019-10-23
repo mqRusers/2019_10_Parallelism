d23dafd8892b830cd5111633a3edd5f5c37535e5
Mr Richard Miller <MQ93612818@mq.edu.au>
2019-10-23 17:47:40 +1100

    initial commit

---
+++ workshop.R
@@ -0,0 +1,88 @@
+#
+# 2019_10_Parallelism
+#
+
+# Let's create some articifal work to be done using R's Random Uniform Distribution function "runif(x)"
+
+
+distribution <- runif(10)
+average <- mean(distribution)
+print(average)
+barplot(distribution)
+
+# now let's set the SEED to get predictable numbers
+set.seed(12345678)
+distribution <- runif(10)
+average <- mean(distribution)
+print(average)
+barplot(distribution)
+
+# And lets create more work (scaling up)
+{
+set.seed(87654321)
+distribution <- runif(100)
+average <- mean(distribution)
+print(average)
+barplot(distribution)
+}
+
+# and how long is that exactly ?
+system.time({
+  set.seed(12345678)
+  distribution <- runif(100e6)
+  average <- mean(distribution)
+  print(average)
+})
+
+
+# Let's wrap this up in a function for our later tests
+workload <- function (seed=0) {
+  set.seed(seed)
+  distribution <- runif(100e6)
+  average <- mean(distribution)
+  return(average)
+} 
+
+# It should give the same result - so let's check
+workload(12345678)
+
+# and take almost the same time to run
+system.time({
+  print(workload(12345678))
+})
+
+
+# Most scientific compute task involve doing similar things over and over.
+# Let's simulate this by computing averages for 10 different SEEDs
+
+workload(0)
+workload(1)
+workload(2)
+workload(3)
+workload(4)
+workload(5)
+workload(6)
+workload(7)
+workload(8)
+workload(9)
+
+# or of course we could use a FOR loop ...
+for (seed in 0:9) {
+  average <- workload(seed)
+  print(average)
+}
+
+# and we could APPLY the workload function to an array of SEEDs
+sapply(0:9, workload)
+
+# but which one is faster ?
+system.time({
+  for (seed in 0:9) {
+    average <- workload(seed)
+    print(average)
+  }
+})
+
+system.time({
+  print(sapply(0:9, workload))
+})
